dabce60cac7448da04a4f4cea4df3807e352f0c8
wademegan96@gmail.com <meganwade@Megans-MacBook-Pro.local>
2020-01-30 22:53:19 -0600

    changed db to database

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8025,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://meganwade@localhost/drip-drop',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://meganwade@localhost/drip-drop',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   }
